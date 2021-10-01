2a6672ee774c5d85b1e3ba31c664ce6afa4d7c65
ljpop <80583587+ljpop@users.noreply.github.com>
2021-10-01 11:54:59 +0200

    HTML CSS - basic

---
+++ main.js
@@ -0,0 +1,24 @@
+// const questions = document.querySelectorAll(".question-box");
+
+// questions.forEach((question) => {
+//   const questionBtn = question.querySelector(".arrow-btn");
+//   const title = question.querySelector("h2");
+//   questionBtn.classList.toggle("rotate");
+
+//   function showAnswer() {
+//     questions.forEach((item) => {
+//       if (item != question) {
+//         item.classList.remove("show");
+//       }
+//     });
+//     question.classList.toggle("show");
+//   }
+
+//   title.addEventListener("click", () => {
+//     showAnswer();
+//   });
+
+//   questionBtn.addEventListener("click", () => {
+//     showAnswer();
+//   });
+// });
